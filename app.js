75e4df5304070e51628a71f04028f635a2634e92
Graham Clapham <info@gcdesign.co.uk>
2014-01-16 22:53:55 +0000

    added mongoose and mongo

--- app.js
+++ app.js
@@ -10,21 +10,29 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+
+// configuration =================
+
+mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); 	// connect to mongoDB database on modulus.io
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger('dev')); 						// log every request to the console
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(express.methodOverride()); 						// simulate DELETE and PUT
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// ----------  app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.bodyParser()); 							// pull information from html in POST
 
 // development only
 if ('development' == app.get('env')) {
